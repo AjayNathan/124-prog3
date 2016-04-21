a40316cb9cfe224f30d9419c25c6928f391fe341
Ajay <ajaynathan@college.harvard.edu>
2016-04-21 18:01:38 -0400

    code

--- code.py
+++ code.py
@@ -42,3 +42,28 @@ def prepartition_move(P):
             j = randnums(0,len(P),1,1)[0]
     P[i] = j
     return P
+
+def max_two(nums):
+    a = 0
+    b = 1
+    if nums[0] < nums[1]:
+            a = 1
+            b = 0
+    for i in xrange(len(nums)):
+            if i > 1:
+                if nums[i] > nums[a]:
+                    b = a
+                    a = i
+                elif nums[i] > nums[b]:
+                    b = i
+    return (a, b)
+
+def kk(nums):
+    for i in xrange(len(nums)):
+        a, b = max_two(nums)
+        nums[a] = nums[a] - nums[b]
+        nums[b] = 0
+
+    a, _ = max_two(nums)
+
+    return nums[a]
